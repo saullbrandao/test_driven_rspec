eae0e9218e13ac94737694d1b51c377dbe4025e7
Saull Brandão <saull@outlook.com>
2022-07-07 16:01:10 -0300

    capybara testing basics with rspec

--- 02_rails_api/config/application.rb
+++ 02_rails_api/config/application.rb
@@ -1,17 +1,17 @@
-require_relative "boot"
+require_relative 'boot'
 
-require "rails"
+require 'rails'
 # Pick the frameworks you want:
-require "active_model/railtie"
-require "active_job/railtie"
-require "active_record/railtie"
+require 'active_model/railtie'
+require 'active_job/railtie'
+require 'active_record/railtie'
 # require "active_storage/engine"
-require "action_controller/railtie"
-require "action_mailer/railtie"
+require 'action_controller/railtie'
+require 'action_mailer/railtie'
 # require "action_mailbox/engine"
 # require "action_text/engine"
-require "action_view/railtie"
-require "action_cable/engine"
+require 'action_view/railtie'
+require 'action_cable/engine'
 # require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
@@ -34,6 +34,6 @@ module RailsApi
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
-    config.api_only = true
+    # config.api_only = true
   end
 end
--- 02_rails_api/config/routes.rb
+++ 02_rails_api/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'home', to: 'home#index'
+  get 'about', to: 'about#index'
   get 'status', to: 'status#index'
   get 'consoles', to: 'consoles#index'
 end
